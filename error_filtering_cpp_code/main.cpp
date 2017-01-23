c3deba8d96af511287cc0d048beeed2e133fe2a1
Nandita Garud <nandita.garud@gmail.com>
2017-01-23 14:11:10 -0800

    error filtering code

---
+++ error_filtering_cpp_code/main.cpp
@@ -0,0 +1,155 @@
+#include<vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <functional>
+#include <algorithm>
+#include <tuple>
+
+#include "stats.hpp"
+#include "trajectory.hpp" 
+
+double calculate_pvalue(Random & random, Trajectory const & trajectory, const int max_num_bootstraps=10000, const int min_numerator_counts=100);
+
+const char delim=',';
+
+int main(int argc, char * argv[]){
+
+    // random number generator
+    // deterministic seed to ensure reproducibility
+    // once pipeline is completed
+    auto random = create_random(42); 
+    
+    // used for reporting purposes
+    int num_processed = 0;
+    int num_passed = 0;
+    int num_surprising = 0;
+    
+    // iterate over all trajectory records in file
+    std::string line;
+    
+    // first print header
+    std::getline(std::cin,line);
+    std::cout << line << std::endl;
+    
+    while(std::getline(std::cin,line)){
+        
+        // parse trajectory record
+        std::stringstream line_stream(line);
+        std::string site_id;
+        std::string item;
+        std::string subitem;
+        
+        std::vector<double> alts;
+        std::vector<double> depths;
+        
+        // read site_id
+        std::getline(line_stream, site_id, '\t');
+        
+        // read alts and depths trajectory
+        auto trajectory = Trajectory();
+        while(std::getline(line_stream, item, '\t')){
+            // loop over samples
+            double alt, depth;
+            std::stringstream item_stream(item);
+            std::getline(item_stream, subitem, ',');
+            alt = std::stof(subitem);
+            std::getline(item_stream, subitem, ',');
+            depth = std::stof(subitem);
+            trajectory.push_back( Sample{ alt, depth } );
+            //std::cerr << alt << "," << depth << " ";
+        }
+        //std::cerr << std::endl;
+        
+        double pvalue;
+            
+        num_processed+=1;
+                
+        if(!passes_filter(trajectory)){
+            pvalue = 2; // shouldn't happen!    
+        }
+        else{
+            // actually calculate autocorrelation score
+            num_passed+=1;
+            pvalue = calculate_pvalue(random, trajectory); 
+        }
+            
+        // increment number of "surprising" trajectories
+        // used only for display purposes during run
+        if(pvalue <= 5e-02){
+            num_surprising += 1; 
+        }
+        
+        if(num_processed % 1000 == 0){
+                std::cerr << num_processed << " trajectories processed, " << num_passed << " passed, " << num_surprising << " surprising!\n";
+        }
+        
+        // print annotated line
+        std::cout << site_id << "|" << pvalue;
+        for(auto const & sample : trajectory){
+            std::cout << "\t" << sample.alt << "," << sample.depth;
+        }
+        std::cout << std::endl;  
+    }
+    
+    std::cerr << "Finished: " << num_processed << " trajectories processed, " << num_passed << " passed, " << num_surprising << " surprising!\n";
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Calculates pvalue for trajectory based on likelihood ratio test
+//
+// Returns: pvalue
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+double calculate_pvalue(Random & random, Trajectory const & observed_trajectory, const int max_num_bootstraps, const int min_numerator_counts){
+     
+    if(!passes_filter(observed_trajectory)){
+        std::cerr << "Trajectory did not pass filter, should not get here!" << std::endl;
+        return 2;
+    }
+    
+    // null model is based on resampling of observed trajectory
+    auto trajectory_generator = BinomialTrajectoryGenerator(observed_trajectory);
+    
+    double observed_LRT =  calculate_LRT(trajectory_generator.unmasked_observed_trajectory);
+    
+    //std::cerr << observed_LRT << std::endl;
+    
+    // calculate # bootstrapped Ts > T
+    int num_greater_LRTs = 0;
+    int current_num_bootstraps = 0;    
+    
+    //std::cerr << "Starting bootstraps!" << std::endl;
+    while(current_num_bootstraps < max_num_bootstraps){
+        
+        current_num_bootstraps+=1;
+        
+        //std::cerr << "Bootstrap " << current_num_bootstraps << std::endl;
+            
+        trajectory_generator.generate_bootstrapped_trajectory(random);
+        
+        // calculate bootstrapped test statistics
+        double bootstrapped_LRT = calculate_LRT(trajectory_generator.unmasked_bootstrapped_trajectory);
+        
+        //std::cerr << bootstrapped_LRT << std::endl;
+        
+        // compare to observed values
+        if( bootstrapped_LRT >= observed_LRT ){
+            // bootstrapped trajectory is at least as extreme
+            num_greater_LRTs+=1;
+        }
+        
+        if( num_greater_LRTs > min_numerator_counts ){ 
+            break;
+        }
+    }   
+    
+    // calculate pvalues
+    double pvalue = (num_greater_LRTs+1.0)/(current_num_bootstraps+1.0);
+    return pvalue;
+}
---
+++ error_filtering_cpp_code/trajectory.hpp
@@ -0,0 +1,189 @@
+#ifndef TRAJECTORY_HPP
+#define TRAJECTORY_HPP
+
+#include<vector>
+#include <iostream>
+#include <random>
+#include <functional>
+#include <algorithm>
+#include <tuple>
+
+#include "stats.hpp"
+
+const double max_error_rate = 0.01;
+const double prevalence_threshold = 0.5;
+
+// A sample could represent a technical replicate, 
+// the sum of all technical replicates for a timepoint
+// or the sum of all techncial replicates for a host
+class Sample{
+    public:
+        double alt;
+        double depth;
+};
+
+// a Trajectory object is just a collection of samples.
+// (order is not important; vector used for performance)
+typedef std::vector<Sample> Trajectory; 
+
+// calculates the average frequency using the binomial MLE
+inline double calculate_average_frequency(Trajectory const & trajectory){
+
+    // first calculate average frequency
+    double total_alts = 0;
+    double total_depths = 0;
+    for(auto const & sample : trajectory){
+        total_alts += sample.alt;
+        total_depths += sample.depth;
+    }
+    double avg_frequency = total_alts*1.0/total_depths;
+    return avg_frequency;
+}
+
+inline double calculate_error_rate(Trajectory const & trajectory){
+    return std::min( calculate_average_frequency(trajectory), max_error_rate);
+}
+
+// We assume that the trajectory has been pre-processed 
+// such that low-coverage samples have depth=0
+inline bool filter_sample(Sample const & sample){
+    if(sample.depth==0){
+        return true;
+    }
+    else{
+        return false;
+    } 
+}
+
+/* Filters out trajectories that do not have at least minimum fraction
+   of covered samples */
+inline bool passes_filter(Trajectory const & trajectory){
+
+    int num_covered_samples = 0;
+    int num_total_samples = 0;
+    for(auto const & sample : trajectory){
+        num_total_samples += 1;
+        if(sample.depth > 0.5){
+            num_covered_samples += 1;
+        }    
+    }
+    
+    if(num_covered_samples > prevalence_threshold*num_total_samples){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+class BinomialAltGenerator{
+    public:
+        int D;
+        double p;
+        BinomialAltGenerator(): D(0), p(0), sample_A(D, p) {};
+        BinomialAltGenerator(int D, double p): D(D), p(p), sample_A(D, p) {};
+        std::binomial_distribution<int> sample_A;
+};
+
+class BinomialTrajectoryGenerator{
+    public:
+    
+        Trajectory bootstrapped_trajectory; // a full bootstrapped replica of the observed trajectory
+        Trajectory unmasked_observed_trajectory; // only non-filtered points in the observed trajectory
+        Trajectory unmasked_bootstrapped_trajectory; // only non-filtered points in the bootstrapped trajectory 
+
+        double p;
+        
+        BinomialTrajectoryGenerator(Trajectory const & trajectory){
+    
+            p = calculate_error_rate(trajectory);
+            
+            //std::cerr << "Error rate = " << p << std::endl;
+            
+            for(int i=0,imax=trajectory.size();i<imax;++i){
+                Sample const & sample = trajectory[i];
+                bootstrapped_trajectory.push_back(sample);
+                if(!filter_sample(sample)){
+                    
+                    unmasked_indices.push_back(i);
+                    unmasked_observed_trajectory.push_back(sample);
+                    unmasked_bootstrapped_trajectory.push_back(sample);
+                    unmasked_alt_generators.push_back(BinomialAltGenerator((int) sample.depth, p));
+                }
+            }
+        };
+        
+        Trajectory & generate_bootstrapped_trajectory(Random & random){
+            int num_attempts = 0;
+            do{
+                num_attempts+=1;
+                //std::cerr << num_attempts << std::endl;
+                // resample alts 
+                for(int i=0,imax=unmasked_bootstrapped_trajectory.size(); i<imax; ++i){
+                    //std::cerr << "Before sampling: " << unmasked_alt_generators[i].D << " " << unmasked_alt_generators[i].p << std::endl;
+                    int new_alt = unmasked_alt_generators[i].sample_A(random);
+                    //std::cerr << new_alt << std::endl;
+                    unmasked_bootstrapped_trajectory[i].alt = new_alt;
+                    bootstrapped_trajectory[unmasked_indices[i]].alt = new_alt;    
+                }
+            }while(!passes_filter(bootstrapped_trajectory));
+            return bootstrapped_trajectory;
+        };
+      
+    private:
+        std::vector<int> unmasked_indices;
+        std::vector<BinomialAltGenerator> unmasked_alt_generators; // used for generating bootstrapped trajectories
+        
+};
+
+// Calculates a (log) likelihood ratio test statistic comparing the null model
+// (all samples are binomial variables with the same avg) vs the alternative
+// (all samples are binomial variables with a sample-specific avg)
+inline double calculate_LRT(Trajectory const & trajectory){
+    
+    
+    // cap the avg frequency at avg_frequency threshold
+    double p = calculate_error_rate(trajectory);
+    
+    double log_LRT = 0;
+    
+    if(p<=0 || p>= 1){
+        return log_LRT;
+    }
+    
+    for(auto const & sample : trajectory){
+    
+        // calculate upper and lower alt thresholds
+        // (so that we don't waste statistical power on
+        //  changes that we don't value scientifically)
+        double expected_alt = sample.depth*p;
+        double stddev_alt = std::sqrt(sample.depth*p*(1-p));
+        
+        double lower_alt_discreteness_threshold = std::floor(expected_alt)-0.5;
+        double lower_alt_stddev_threshold = expected_alt-stddev_alt;
+        double lower_alt_threshold = std::min(lower_alt_discreteness_threshold, lower_alt_stddev_threshold);
+        lower_alt_threshold = std::max(lower_alt_threshold, 0.0);
+    
+        double upper_alt_discreteness_threshold = std::ceil(expected_alt)+0.5;
+        double upper_alt_stddev_threshold = expected_alt+stddev_alt;
+        double upper_alt_threshold = std::max(upper_alt_discreteness_threshold, upper_alt_stddev_threshold);
+        upper_alt_threshold = std::min(upper_alt_threshold, sample.depth);
+    
+        if( (sample.alt < lower_alt_threshold) || (sample.alt > upper_alt_threshold) ){
+        
+            double pi = sample.alt/sample.depth;
+            
+            if(pi>0){
+                log_LRT += sample.depth*pi*std::log( pi/p ); 
+            }
+            
+            if(pi<1){
+                log_LRT += sample.depth*(1-pi)*std::log( (1-pi)/(1-p) );
+            }
+        }
+    }
+    
+    return log_LRT;
+}
+
+#endif
