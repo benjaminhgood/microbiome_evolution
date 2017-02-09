7ab0fcf7ef16b615d17097cd1365126ce55ef6e9
ngarud <nandita.garud@gmail.com>
2017-02-08 21:42:07 -0800

    now can run hap script

--- bash_scripts/run_diversity_scripts.sh
+++ bash_scripts/run_diversity_scripts.sh
@@ -1,10 +1,12 @@
 species=$1
 
-python ~/projectBenNandita/plot_hmp_metadata.py     
-python ~/projectBenNandita/plot_coverage_distribution.py $species
-python ~/projectBenNandita/plot_pooled_sfs.py $species
-python ~/projectBenNandita/plot_spatial_pi.py $species
-python ~/projectBenNandita/plot_pNpS_vs_pi.py $species
-python ~/projectBenNandita/plot_gene_ld.py $species
-python ~/projectBenNandita/plot_pi_distribution.py $species 
-python ~/projectBenNandita/print_distance_matrix.py $species
+#python ~/projectBenNandita/plot_hmp_metadata.py     
+#python ~/projectBenNandita/plot_coverage_distribution.py $species
+#python ~/projectBenNandita/plot_pooled_sfs.py $species
+#python ~/projectBenNandita/plot_spatial_pi.py $species
+#python ~/projectBenNandita/plot_pNpS_vs_pi.py $species
+#python ~/projectBenNandita/plot_gene_ld.py $species
+#python ~/projectBenNandita/plot_pi_distribution.py $species 
+#python ~/projectBenNandita/print_distance_matrix.py $species
+
+python ~/projectBenNandita/plot_gene_haplotypes.py $species
