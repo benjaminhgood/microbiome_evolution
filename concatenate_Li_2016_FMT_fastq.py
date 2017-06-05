86ce53c9df7b3bc8b9ab1a328f618a49888f9336
ngarud <nandita.garud@gmail.com>
2017-06-04 21:32:05 -0700

    merge replicates of DON-11

--- concatenate_Li_2016_FMT_fastq.py
+++ concatenate_Li_2016_FMT_fastq.py
@@ -8,6 +8,9 @@ parser.add_argument("subject_id", help="name of subject to process")
 args = parser.parse_args()
 subject = args.subject_id
 
+if subject.split('-')[0] == 'FAT_DON_11':
+    subject='FAT_DON_11'
+
 # dictionary with all the fastq file names for the sample of interest
 files={}
 inFN="/netapp/home/ngarud/shattuck/BenNanditaProject/MIDAS_intermediate_files_Li_2016_FMT/Li_2016_FMT_paired_accessions.txt"
@@ -17,6 +20,9 @@ for line in inFile:
     items=line.strip().split()
     accession=items[0]
     subject_id=items[1]
+    subject_id_split=subject_id.split('-')
+    if subject_id_split[0]=='FAT_DON_11':
+        subject_id=subject_id_split[0]
     if subject_id not in files:
         files[subject_id]=[]
     files[subject_id].append(accession)
