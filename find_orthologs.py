8f4162c7f91bdb85e698b0ca86bf7cfa60a42b2b
ngarud <nandita.garud@gmail.com>
2017-06-19 08:30:53 -0700

    finds orthologs matching bit score of species of interest

--- find_orthologs.py
+++ find_orthologs.py
@@ -37,7 +37,9 @@ species_name = args.species_name
 # read in ortholog information       #
 ######################################      
 def read_ortholog_information(infile):
-    gene_statistics={}
+
+    gene_statistics_tmp={}
+    gene_statistics_tmp_2={}
     matching_genome_list={}
     num_genes_with_match=0 # I am storing the number of genes with a match to any species other than the species of interest. THe match must be 100%
     for line in infile:
@@ -45,20 +47,34 @@ def read_ortholog_information(infile):
         gene=items[0]
         matching_genome=items[1]
         percent_id=float(items[2])
-        if gene not in gene_statistics:
-            gene_statistics[gene]=[0,0,0] # first is for number of genomes matching, second is for number of genomes matching with 100%, # thurs is for whether the speceis matches at 100%.
-        if percent_id == 100.00:
-            if matching_genome==species_name:
-                gene_statistics[gene][2]+=1
-            else:
-                gene_statistics[gene][1]+=1
-                num_genes_with_match +=1
-                if matching_genome not in matching_genome_list:
-                    matching_genome_list[matching_genome]=1
-                else:
-                    matching_genome_list[matching_genome]+=1
-        else:
-            gene_statistics[gene][0]+=1
+        bit_score=float(items[3])
+        if gene not in gene_statistics_tmp:
+            gene_statistics_tmp[gene]={} 
+            gene_statistics_tmp_2[gene]={}
+        gene_statistics_tmp[gene][matching_genome]=bit_score
+        gene_statistics_tmp_2[gene][matching_genome]=percent_id
+    gene_statistics={}
+    for gene in gene_statistics_tmp:
+        gene_statistics[gene]=[0,0] # first is for number of genomes matching, second is for number of genomes matching with same bit score as species_name.
+        gene_statistics[gene][0]=len(gene_statistics_tmp[gene])
+        species_bit_score=gene_statistics_tmp[gene][species_name]
+        print 'percent id species_name'
+        print gene_statistics_tmp_2[gene][species_name]
+        print 'species_name bit score is ' + str(species_bit_score)
+        for matching_genome in gene_statistics_tmp[gene]:
+            if matching_genome!=species_name:
+                if gene_statistics_tmp[gene][matching_genome]>=species_bit_score:
+                    print 'percent_id ' + matching_genome
+                    print gene_statistics_tmp_2[gene][matching_genome]
+                    print matching_genome + ' bit score is ' + str(gene_statistics_tmp[gene][matching_genome])
+                    gene_statistics[gene][1]+=1
+                    if matching_genome not in matching_genome_list:
+                        matching_genome_list[matching_genome]=1
+                    else:
+                        matching_genome_list[matching_genome]+=1
+        if gene_statistics[gene][1] >=1:
+            num_genes_with_match +=1
+
     return gene_statistics, matching_genome_list, num_genes_with_match
 
 #####################
@@ -86,7 +102,7 @@ width = 1.0
 df=pandas.DataFrame(gene_statistics_within_host)
 df=df.transpose()
 num_genes_with_match_within_host=(df[1]!= 0).sum() 
-df=df.rename(columns = {0:'< 100%, non-ref match', 1: '100%, non-ref match', 2: '100%, ref match'})
+df=df.rename(columns = {0:'< 100%, non-ref match', 1: '100%, non-ref match'})
 ax =df.plot(kind='barh', stacked=True, figsize=(6,10), title=species_name, width=width)
 ax.set_xlabel("Number of orthologs")
 ax.set_ylabel("Gene")
@@ -106,7 +122,7 @@ matplotlib.rcParams['ytick.labelsize'] = label_size
 pos = numpy.arange(len(df.index))
 width = 1.0 
 
-ax =df.plot(kind='barh', stacked=True, figsize=(6,10), title=species_name, width=width)
+ax =df.plot(kind='barh', stacked=True, figsize=(6,10), title=species_name, width=width, color=['b','r'])
 ax.set_xlabel("Number of genes")
 ax.set_ylabel("Species")
 
@@ -125,8 +141,8 @@ matplotlib.rcParams['ytick.labelsize'] = label_size
 df=pandas.DataFrame(gene_statistics_random)
 df=df.transpose()
 num_genes_with_match_random=(df[1]!= 0).sum()
-df=df.rename(columns = {0:'< 100%, non-ref match', 1: '100%, non-ref match', 2: '100%, ref match'})
-ax =df.plot(kind='barh', stacked=True, figsize=(6,10), title=species_name, width=width)
+df=df.rename(columns = {0:'< 100%, non-ref match', 1: '100%, non-ref match'})
+ax =df.plot(kind='barh', stacked=True, figsize=(6,10), title=species_name, width=width, color=['b','r'])
 ax.set_xlabel("Number of orthologs")
 ax.set_ylabel("Gene")
 
@@ -147,7 +163,7 @@ matplotlib.rcParams['ytick.labelsize'] = label_size
 pos = numpy.arange(len(df.index))
 width = 1.0 
 
-ax =df.plot(kind='barh', stacked=True, figsize=(6,10), title=species_name, width=width)
+ax =df.plot(kind='barh', stacked=True, figsize=(6,10), title=species_name, width=width,color=['b','r'])
 ax.set_xlabel("Number of genes")
 ax.set_ylabel("Species")
 
